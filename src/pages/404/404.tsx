2b677ac0aad7b12b9b134fd9824512ca41f8c7da
termorey <is_front@mail.ru>
2022-05-24 17:49:12 +0300

    Обновление: - 404 отрисовка

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import style from "./style.module.scss";
 import Block from "../../components/block/block";
+import {Link} from "react-router-dom";
+import {multiClass} from "../../utils/utils";
 
 const NotFound = () => {
     return (
-        <Block>
-            Page not found
-        </Block>
+        <main className={style.notFoundPage}>
+            <Block className={multiClass(style.notFoundBlock, "background padding bordered backdropBlur")}>
+                <span>Страница не найдена</span>
+                <Link to={"/"}>Вернуться на главную</Link>
+            </Block>
+        </main>
     )
 }
 
