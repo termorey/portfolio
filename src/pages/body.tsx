9316272358e68fe5b7f88ba84cc6fc692e00d4d0
termorey <is_front@mail.ru>
2022-05-24 20:31:43 +0300

    Обновлено: - Роутеринг для страницы контактов

--- src/pages/body.tsx
+++ src/pages/body.tsx
@@ -3,13 +3,18 @@ import {Routes, Route} from "react-router-dom";
 import Index from "./index";
 import Menu from "../components/menu/menu";
 import NotFound from "./404/404";
+import Contacts from "./contacts/contacts";
 
 const Body = () => {
     return (
         <>
             <Menu/>
             <Routes>
-                <Route path={"/"} element={<Index/>} />
+                <Route path={"/"}>
+                    <Route path={""} element={<Index/>} />
+                    <Route path={"contacts"} element={<Contacts/>} />
+                    <Route path={"*"} element={<NotFound/>} />
+                </Route>
                 <Route path={"*"} element={<NotFound/>} />
             </Routes>
         </>
