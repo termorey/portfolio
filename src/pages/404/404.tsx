0fb5b6fae99e48a75e7df4686f01a84021752c69
termorey <is_front@mail.ru>
2022-05-24 13:29:02 +0300

    Добавлена: - Страница ошибки 404

---
+++ src/pages/404/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.module.scss";
+
+const NotFound = () => {
+    return (
+        <Block>
+            Page not found
+        </Block>
+    )
+}
+
+export default NotFound;
